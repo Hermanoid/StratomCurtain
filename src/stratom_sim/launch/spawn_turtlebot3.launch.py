118a4320dd2ba03b46bd6e973c1d468d7efddcf7
Lucas Niewohner <lucasniewohner@gmail.com>
2023-05-25 13:19:10 -0600

    Added static transform publisher so the "map" frame exists now

--- src/stratom_sim/launch/spawn_turtlebot3.launch.py
+++ src/stratom_sim/launch/spawn_turtlebot3.launch.py
@@ -25,54 +25,43 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get the urdf file
-    TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-    model_folder = 'turtlebot3_' + TURTLEBOT3_MODEL
-    urdf_path = os.path.join(
-        get_package_share_directory('stratom_sim'),
-        'models',
-        model_folder,
-        'model.sdf'
-    )
+    TURTLEBOT3_MODEL = os.environ["TURTLEBOT3_MODEL"]
+    model_folder = "turtlebot3_" + TURTLEBOT3_MODEL
+    urdf_path = os.path.join(get_package_share_directory("stratom_sim"), "models", model_folder, "model.sdf")
 
     # Launch configuration variables specific to simulation
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    x_pose = LaunchConfiguration("x_pose", default="0.0")
+    y_pose = LaunchConfiguration("y_pose", default="0.0")
 
     # Declare the launch arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    
-    declare_x_position_cmd = DeclareLaunchArgument(
-        'x_pose', default_value='0.0',
-        description='Specify namespace of the robot')
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
 
-    declare_y_position_cmd = DeclareLaunchArgument(
-        'y_pose', default_value='0.0',
-        description='Specify namespace of the robot')
+    declare_x_position_cmd = DeclareLaunchArgument("x_pose", default_value="0.0", description="Specify namespace of the robot")
+
+    declare_y_position_cmd = DeclareLaunchArgument("y_pose", default_value="0.0", description="Specify namespace of the robot")
 
     start_gazebo_ros_spawner_cmd = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', TURTLEBOT3_MODEL,
-            '-file', urdf_path,
-            '-x', x_pose,
-            '-y', y_pose,
-            '-z', '0.01'
-        ],
-        output='screen',
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", TURTLEBOT3_MODEL, "-file", urdf_path, "-x", x_pose, "-y", y_pose, "-z", "0.01"],
+        output="screen",
     )
 
     # Add odom_tf2_adapter node to convert ground-truth simulation pose information to tf transform
     # This node basically says "Take the position from the simulation and use it directly"
     odom_tf2_adapter_node = Node(
-        package='odom_tf2_adapter',
-        executable='odom_tf2_adapter',
-        name='odom_tf2_adapter',
-        parameters=[{"odom_topic": "/odom",
-                     "base_frame": "base_footprint",
-                     "parent_frame": "odom"}]
+        package="odom_tf2_adapter",
+        executable="odom_tf2_adapter",
+        name="odom_tf2_adapter",
+        parameters=[{"odom_topic": "/odom", "base_frame": "base_footprint", "parent_frame": "odom"}],
     )
-    
+
+    static_tf2_broadcaster_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        arguments=["--frame-id", "map", "--child-frame-id", "odom"],
+    )
 
     ld = LaunchDescription()
 
@@ -85,5 +74,6 @@ def generate_launch_description():
 
     # Add the nodes to launch
     ld.add_action(odom_tf2_adapter_node)
+    ld.add_action(static_tf2_broadcaster_node)
 
     return ld
